b0721bae4e74d0e288315effeb4afc5b0a9194d8
codewithrich <103169939+rseyum@users.noreply.github.com>
2022-08-10 17:55:24 +0000

    updates-api

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
 <title>Bond Web Service Demo</title>
 <style>
 	body {font-family:georgia;}
-
-
-
-.film{
+  
+   .film{
     border:1px solid #E77DC2;
     border-radius: 5px;
     padding: 5px;
@@ -18,18 +16,19 @@
     position:absolute;
     right:10px;
     top:10px;
-
+  }
   
-
+  .pic img{
+	max-width:115px;
+  }
 </style>
 <script src="https://code.jquery.com/jquery-latest.js"></script>
 
 <script type="text/javascript">
-
-function bondTempate(){
-return `
-<div class="film">
-      <b>Film</b>:${film.Film}<br/>
+  function bondTemplate(film){
+    return `
+    <div class="film">
+            <b>Film</b>:${film.Film}<br/>
        <b>Title</b>:${film.Title}<br/>
        <b>Year</b>:${film.Year}<br/>
        <b>Director</b>:${film.Director}<br/>
@@ -39,14 +38,13 @@ return `
        <b>Bond</b>:${film.Bond}<br/>
        <b>Budget</b>:${film.Budget}<br/>
        <b>BoxOffice</b>:${film.BoxOffice}<br/>
-    <div class="pic"><img src="thubnais/${film.Image}"</div>
-
-`;
-  
-}
+            <div class="pic"><img src="images/${film.Image}" /></div>
+      </div>
+    `;
+    
+  }
   
 $(document).ready(function() { 
- 
  $('.category').click(function(e){
    e.preventDefault(); //stop default action of the link
    cat = $(this).attr("href");  //get category from URL
@@ -57,54 +55,36 @@ $(document).ready(function() {
      dataType: "json"
    });
    request.done(function( data ) {
-     console.log(data);
-     // place data.title on page
+    console.log(data);
+     // place data.title on page 
      $("#filmtitle").html(data.title);
-     // clear previous films
+     // clear previous data
      $("#films").html("");
-     // loop through data.films and place on page 
+     // loop thru data.films and place on page
      $.each(data.films,function(i,item){
-         let myData = bondTemplet(item);
-         $("<div></div>").html(myData).appendTo("#films")
+      let myData = bondTemplate(item);
+       $("<div></div>").html(myData).appendTo("#films");
        
      });
-    /*
-     let myData = JSON.stringify(data,null,4);
-     myData = "<pre>" + myData + "</pre>";
-     $("#output").html(myData);
+     // let myData = JSON.stringify(data,null,4);
+     // myData = "<pre>" + myData + "</pre>";
+     // $("#output").html(myData);
    });
-   */
-   
    request.fail(function(xhr, status, error ) {
 alert('Error - ' + xhr.status + ': ' + xhr.statusText);
    });
- 
   });
 }); 
 </script>
 </head>
 	<body>
-	<h1>Bond Web Service</h1>
+<h1>Bond Web Service</h1>
 		<a href="year" class="category">Bond Films By Year</a><br />
 		<a href="box" class="category">Bond Films By International Box Office Totals</a>
 		<h3 id="filmtitle">Title Will Go Here</h3>
 		<div id="films">
-      <!--
-			<div class ="film">
-         <b>Film </b> : 1 <br/>
-         <b>Title </b> : Dr. No <br/>
-         <b>Year</b> : 1962 <br/>
-         <b>Director </b> : Terence Young <br/>
-         <b>Producers </b> : Harry Saltzman and Albert R. Broccoli <br/>
-         <b>Writers </b> : Richard Maibaum, Johanna Harwood and Berkely Mather <br/>
-         <b>Composer </b> : Monty Norman <br/>
-         <b>Bond </b> : Sean Connery <br/>
-         <b>Budget </b> : $1,000,000.00 <br/>
-         <b>BoxOffice </b> : $59,567,035.00 <br/>
-        <div class="pic"> <img src="thumbnails/dr-no.jpg"></div>
-      </div>
-      -->
+
 		</div>
-		<div id="output">Results go here</div>
+		<div id="output"></div>
 	</body>
 </html>
